be99e8ee6e46bad8223f8fb82a3bcf71b3cac47b
Sean Riedl <smriedl@gmail.com>
2021-06-19 13:22:02 -0400

    Add javascript functions for adding books, displaying library and form

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let myLibrary = [1, 2, 3 ,4];
+let libraryContainer = document.getElementById("library")
 let addForm = document.getElementById("Add-book-form");
 let addBookButton = document.getElementById("new-book");
 let displayLibraryButton = document.getElementById("display-books")
@@ -20,13 +21,57 @@ function addBookToLibray(event) {
 
   newBook = new Book(author, title, pageNumber, read);
   myLibrary.push(newBook);
+  addBookToDocument(newBook);
   addForm.style.display = "none";
   addForm.reset();
+  libraryContainer.style.display = "flex";
 }
 addForm.addEventListener("submit", addBookToLibray);
 
+// Add new book to HTML document
+function addBookToDocument(newBook) {
+  const bookContainer = document.createElement("div");
+  bookContainer.className = "book";
+
+  const title = document.createElement("H1");
+  const author = document.createElement("H2");
+  const pageCount = document.createElement("p");
+  
+  const label = document.createElement("label");
+  const labelText = document.createTextNode("Read: ");
+  label.appendChild(labelText);
+  
+  var readCheckbox = document.createElement("INPUT");
+  readCheckbox.setAttribute("type", "checkbox");
+  
+  const bookTitle = document.createTextNode(newBook.title);
+  const bookAuthor = document.createTextNode(newBook.author);
+  const bookPageCount = document.createTextNode(newBook.pageNumber);
+  
+  title.appendChild(bookTitle);
+  author.appendChild(bookAuthor);
+  pageCount.appendChild(bookPageCount);
+  title.appendChild(bookTitle);
+
+  if (newBook.read === true) {
+    readCheckbox.checked = true;
+  } else {
+    readCheckbox.checked = false;
+  }
+
+  bookContainer.appendChild(title);
+  bookContainer.appendChild(author);
+  bookContainer.appendChild(pageCount);
+  bookContainer.appendChild(label);
+  bookContainer.appendChild(readCheckbox);
+  libraryContainer.appendChild(bookContainer);
+}
+
 // Display add book form when clicking "Add a Book" button
 function showAddBookForm() {
+  if (libraryContainer.style.display == "flex") {
+    libraryContainer.style.display = "none";
+  }
   if (addForm.style.display == "none") {
     addForm.style.display = "block";
   } else { addForm.style.display = "none" }
@@ -35,13 +80,12 @@ addBookButton.addEventListener("click", showAddBookForm);
 
 // Display library when clicking "Show Library" button
 function showLibrary() {
-  myLibrary.forEach(function(book) {
-    const newBookElement = document.createElement("div");
-
-    const newAuthor = document.createTextNode("Author");
-    newBookElement.appendChild(newAuthor);
-
-    document.body.insertBefore(newBookElement, addForm);
-  })
+  if (addForm.style.display == "block") {
+    addForm.style.display = "none";
+  }
+  if (libraryContainer.style.display == "none") {
+    libraryContainer.style.display = "flex";
+  } else { libraryContainer.style.display = "none" }
 }
+
 displayLibraryButton.addEventListener("click", showLibrary);
