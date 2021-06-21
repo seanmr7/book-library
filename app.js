56276e8632a217c868436770ce180c00a8133368
Sean Riedl <smriedl@gmail.com>
2021-06-21 19:03:28 -0400

    Add local storage and refractor changeReadBox function

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let myLibrary = [];
+//let myLibrary = [];
+setLibrary();
 let libraryContainer = document.getElementById("library")
 let addForm = document.getElementById("Add-book-form");
 let addBookButton = document.getElementById("new-book");
@@ -15,7 +16,6 @@ Book.prototype.readStatus = function() {
   if (this.read == false) {
     this.read = true;
   } else { this.read = false; }
-  alert("Read status changed")
 }
 
 // Add new book to library array when addForm is submitted
@@ -29,6 +29,7 @@ function addBookToLibray(event) {
   newBook = new Book(author, title, pageNumber, read);
   myLibrary.push(newBook);
   addForm.reset();
+  populateStorage();
   showLibrary();
 }
 addForm.addEventListener("submit", addBookToLibray);
@@ -56,6 +57,7 @@ function showLibrary() {
   } else { libraryContainer.style.display = "none" }
 }
 
+// Removes book div elements and repopulates library div
 function resetLibrary() {
   libraryContainer.innerHTML = '';
   addBooksToDocument(myLibrary);
@@ -109,18 +111,58 @@ function deleteBook() {
   bookIndex = this.id
   myLibrary.splice(bookIndex, 1);
   resetLibrary();
+  populateStorage();
 }
 
 function changeReadBox() {
   book = myLibrary[this.parentElement.getAttribute("data-book-index")]
-  if (this.checked) {
-    book.readStatus();
-  } else { book.readStatus() }
+  if (this.checked && book.read === false) {
+    book.read = true;
+  } else { book.read = false }
+  populateStorage();
 }
 
+// storageAvailable from mozilla developer https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+function storageAvailable(type) {
+  var storage;
+  try {
+      storage = window[type];
+      var x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+  }
+  catch(e) {
+      return e instanceof DOMException && (
+          // everything except Firefox
+          e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          // acknowledge QuotaExceededError only if there's something already stored
+          (storage && storage.length !== 0);
+  }
+}
 
+// Set the libray using local storage
+function setLibrary() {
+  if (!localStorage.getItem('myLibrary')) {
+    myLibrary = [];
+  } else { myLibrary = JSON.parse(localStorage.getItem('myLibrary')) }
+}
+
+function populateStorage() {
+  myLibrayStorage = JSON.stringify(myLibrary)
+  localStorage.setItem('myLibrary', myLibrayStorage);
+}
+
+/*
 let LOTR = new Book("JRR Tolkien", "Lord of the Rings", "355", true);
 let HarryPotter = new Book("JK Rowling", "Harry Potter", "309", true);
 let theGiver = new Book("Lois Lowry", "The Giver", "150", false);
 
-myLibrary.push(LOTR, HarryPotter, theGiver)
+myLibrary.push(LOTR, HarryPotter, theGiver) */
